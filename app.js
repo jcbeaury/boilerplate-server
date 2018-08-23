367855a7718f8580fb782f730e29d298273bb23c
jcbeaury <jcbeaury@gmail.com>
2018-08-23 11:05:33 -0400

    Add jwt auth

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
+// init
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,6 +12,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// auth
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://hello-jcb.auth0.com/.well-known/jwks.json"
+  }),
+  audience: 'boilerplate-server',
+  issuer: "https://hello-jcb.auth0.com/",
+  algorithms: ['RS256']
+});
+app.use(jwtCheck);
+
+// routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
