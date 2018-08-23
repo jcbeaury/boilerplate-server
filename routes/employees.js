13f1d14f5cd1265f9bc7bcafbe23daf4303dec63
jcbeaury <jcbeaury@gmail.com>
2018-08-23 16:38:38 -0400

    Fix typo in mock data

--- routes/employees.js
+++ routes/employees.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET all employees */
 router.get('/', function(req, res, next) {
-  res.send({amployees: [{name: 'Employee 1'}, {name: 'Employee 2'}, {name: 'Employee 3'}, {name: 'Employee 4'}, {name: 'Employee 5'}]});
+  res.send({employees: [{name: 'Employee 1'}, {name: 'Employee 2'}, {name: 'Employee 3'}, {name: 'Employee 4'}, {name: 'Employee 5'}]});
 });
 
 module.exports = router;
